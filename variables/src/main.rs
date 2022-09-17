33906dc80db99180b4511d2305d1abf46607d099
GravesStudios <91075234+GravesStudios@users.noreply.github.com>
2022-09-17 13:14:56 -0500

    I have deleted a line of code here

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,7 +3,4 @@ fn main() {
     println!("The Value of X is {x}");
     x = 6;
     println!("The Value of X is {x}");
-
-    // This can never be mutable. 
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 }
